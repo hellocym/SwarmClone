ee1dd55c8102c408740ead9902dc3f000059b939
kyv001 <kyvyang@foxmail.com>
2024-12-24 23:55:35 +0800

    加入数据集接口和余弦退火学习率调度器

--- swarmclone/config.py
+++ swarmclone/config.py
@@ -2,5 +2,21 @@ from dataclasses import dataclass, field
 
 @dataclass
 class Config:
+    # Tokenizer参数
     VOCAB_SIZE: int = 32768
     SPECIAL_TOKENS: dict[str, int] = field(default_factory=lambda: {"<pad>": 0, "<eos>": 1, "<unk>": 2})
+    
+    # 模型参数
+    MAX_LENGTH: int = 1024
+    MODEL_DIM: int = 1024
+    NUM_HEADS: int = 12
+    NUM_LAYERS: int = 16
+    DROPOUT: float = 0.1
+
+    # 训练参数
+    BATCH_SIZE: int = 1
+    N_BATCHES_PER_STEP: int = 100
+    MAX_LEARNING_RATE: float = 1e-4
+    MIN_LEARNING_RATE: float = 1e-6
+    WARMUP_STEPS: int = 10000
+    TOTAL_STEPS: int = 1000000
---
+++ swarmclone/llm/dataset.py
@@ -0,0 +1,44 @@
+from torch.utils.data import Dataset
+import torch
+import numpy as np
+from . import config
+
+class PreTrainDataset(Dataset):
+    def __init__(self, data_path: str, max_lenth: int):
+        self.data_path = data_path
+        self.max_lenth = max_lenth
+        data = np.fromfile(self.data_path, dtype=np.uint16)
+        self.n_lines = data.shape[0] // (max_lenth + 1)
+        data = data[:self.n_lines * (max_lenth + 1)]
+        self.data = data.reshape((self.n_lines, max_lenth + 1))
+
+    def __len__(self):
+        return self.n_lines
+
+    def __getitem__(self, index):
+        line = self.data[index]
+        x = torch.from_numpy(line[:-1])
+        y = torch.from_numpy(line[1:])
+        return x, y
+
+if __name__ == '__main__':
+    import sys
+    from torch.utils.data import DataLoader
+    from tokenizers import Tokenizer # type: ignore
+    if len(sys.argv) < 3:
+        print("Usage: python -m swarmclone.llm.dataset <tokenizer_path> <data_path>")
+        exit(1)
+    tokenizer_path = sys.argv[1]
+    data_path = sys.argv[2]
+    tokenizer = Tokenizer.from_file(tokenizer_path)
+    dataset = PreTrainDataset(data_path, config.MAX_LENGTH)
+    dataloader = DataLoader(dataset, batch_size=config.BATCH_SIZE, shuffle=True)
+    for x, y in dataloader:
+        print(x)
+        print(y)
+        print(tokenizer.decode(x[0].tolist()))
+        print(tokenizer.decode(y[0].tolist()))
+        try:
+            input()
+        except EOFError:
+            break

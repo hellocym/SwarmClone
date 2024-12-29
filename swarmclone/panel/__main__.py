faf5ed74940260e1b68779c6d04cf3d43ab2b0ff
郝某人 <1218271192@qq.com>
2024-12-29 21:30:11 +0800

    加入bootstrap作为前端UI库
    包含一个简单的示例HTML页面作为测试

--- swarmclone/panel/__main__.py
+++ swarmclone/panel/__main__.py
@@ -1,12 +1,19 @@
-from fastapi import FastAPI, Request # type: ignore
-from . import config # 正确示例
+import os
+from fastapi import FastAPI, Request  # type: ignore
+from . import config  # 正确示例
 from fastapi.staticfiles import StaticFiles  # type: ignore
 from fastapi.templating import Jinja2Templates  # type: ignore
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="frontend/"), name="static")
 
-templates = Jinja2Templates(directory="frontend/static")
+# 获取当前项目的根目录
+base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+
+# 设置静态文件目录和模板目录
+static_dir = os.path.join(base_dir, "frontend/static")
+
+app.mount("/static", StaticFiles(directory=static_dir), name="static")
+templates = Jinja2Templates(directory=static_dir)
 
 @app.get("/")
 def root(request: Request):

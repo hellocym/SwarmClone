9272b93cc463f5686b4c922b1d75dd0e5a97894d
kyv001 <kyvyang@foxmail.com>
2024-12-31 23:35:14 +0800

    让网络设置更合理

--- swarmclone/config.py
+++ swarmclone/config.py
@@ -7,27 +7,11 @@ class Config:
     DEVICE: str = "cuda"
 
     # 网络配置
-    LLM_HOST: str = "localhost"
-    LLM_TO_PANEL: int = 1000
-    LLM_FROM_PANEL: int = 1001
-
-    ASR_HOST: str = "localhost"
-    ASR_TO_PANEL: int = 2000
-
-    TTS_HOST: str = "localhost"
-    TTS_FROM_PANEL: int = 3000
-
-    CHAT_HOST: str = "localhost"
-    CHAT_TO_PANEL: int = 4000
-
-    UNITY_HOST: str = "localhost"
-    UNITY_FROM_PANEL: int = 5000
-
     PANEL_HOST: str = "localhost"
     PANEL_WEB_PORT: int = 8080
-    PANEL_TO_LLM: int = 6000
-    PANEL_TO_TTS: int = 6001
-    PANEL_TO_UNITY: int = 6002
-    PANEL_FROM_LLM: int = 6003
-    PANEL_FROM_ASR: int = 6004
-    PANEL_FROM_CHAT: int = 6005
+    PANEL_TO_LLM: int = 8000
+    PANEL_TO_TTS: int = 8001
+    PANEL_TO_UNITY: int = 8002
+    PANEL_FROM_LLM: int = 8003
+    PANEL_FROM_ASR: int = 8004
+    PANEL_FROM_CHAT: int = 8005
